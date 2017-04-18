fa35f636830548508138649054aae924d6ccf382
dsay <dsay@ciklum.com>
2017-04-18 17:02:40 +0300

    Initial Commit

---
+++ POPDataSource/POPDataSource.h
@@ -0,0 +1,19 @@
+//
+//  POPDataSource.h
+//  POPDataSource
+//
+//  Created by User on 4/18/17.
+//  Copyright © 2017 Dima Sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for POPDataSource.
+FOUNDATION_EXPORT double POPDataSourceVersionNumber;
+
+//! Project version string for POPDataSource.
+FOUNDATION_EXPORT const unsigned char POPDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POPDataSource/PublicHeader.h>
+
+
